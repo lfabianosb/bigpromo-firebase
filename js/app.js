d96bb6e2f2464de9ffe37632ab7c19f58d74bbca
Fabiano Bandeira <lfabianosb@gmail.com>
2017-04-16 22:14:40 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAcgQ6CUVgzTPqb9V2NBC2YLNR6RGIVZlU",
+	authDomain: "bigpromo-99017.firebaseapp.com",
+	databaseURL: "https://bigpromo-99017.firebaseio.com",
+	projectId: "bigpromo-99017",
+	storageBucket: "bigpromo-99017.appspot.com",
+	messagingSenderId: "675219432579"
+};
+firebase.initializeApp(config);
